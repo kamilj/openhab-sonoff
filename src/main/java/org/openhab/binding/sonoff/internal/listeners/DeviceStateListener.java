3eb85df20a3099bffa330b7c6dd63d5949abe316
Delid4ve <56836577+delid4ve@users.noreply.github.com>
2021-02-04 12:14:28 +0000

    Add files via upload

---
+++ src/main/java/org/openhab/binding/sonoff/internal/listeners/DeviceStateListener.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.sonoff.internal.listeners;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openhab.binding.sonoff.internal.dto.api.Device;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Informs about updated sensor states
+ *
+ * @author David Graeff - Initial contribution
+ */
+@NonNullByDefault
+public interface DeviceStateListener {
+
+    void lanUpdate(JsonObject jsonObject, String ipaddress, String sequence);
+
+    void cloudUpdate(Device device);
+}
