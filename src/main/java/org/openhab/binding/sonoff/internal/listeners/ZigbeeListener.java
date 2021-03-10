5e8af556d60c4920c4851154066e70c47ff51461
Delid4ve <56836577+delid4ve@users.noreply.github.com>
2021-03-10 13:48:04 +0000

    Delete src/main directory

--- src/main/java/org/openhab/binding/sonoff/internal/listeners/ZigbeeListener.java
+++
@@ -1,9 +0,0 @@
-package org.openhab.binding.sonoff.internal.listeners;
-
-import org.eclipse.jdt.annotation.NonNullByDefault;
-
-@NonNullByDefault
-public interface ZigbeeListener {
-
-    void sensorTriggered(String date);
-}
